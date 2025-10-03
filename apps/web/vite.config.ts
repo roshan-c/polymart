1047abb65bf4936feaa97eab3a5babcc10f0b789
roshan-c <hhhhhhhhh333365@gmail.com>
2025-10-03 10:25:04 +0100

    Add aggressive cache control headers to fix Chrome caching issue

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
 });
